9c9f571658a81537de78f63c7df5816cf383edcb
Souma061 <soumabrataghosh57@gmail.com>
2025-09-05 00:02:19 +0530

    type error fixes in app.js

--- src/app.js
+++ src/app.js
@@ -6,10 +6,10 @@ const app = express();
 
 app.use(cors({
   origin: process.env.CORS_ORIGIN,
-  Credentials: true
+  credentials: true
 }));
 app.use(express.json({limit: "15kb"}));
-app.use(express.urlencoded({extended:"true",limit: "15kb"}));
+app.use(express.urlencoded({extended:true,limit: "15kb"}));
 app.use(express.static("public"));
 
 app.use(cookieParser());
