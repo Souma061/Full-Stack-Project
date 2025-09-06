7a764382bd3b14e377e50646d76bbefebd3aee39
Souma061 <soumabrataghosh57@gmail.com>
2025-09-06 12:45:37 +0530

    Cloudinary,multer configs and project configs ready

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,27 @@
+import {v2 as cloudinary} from 'cloudinary';
+import fs from 'fs';
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+
+const uploadOnCloudinary = async (localFilePath) => {
+  try {
+    if(!localFilePath) return;
+    const response = await cloudinary.uploader.upload(localFilePath, {
+      resource_type:"auto",
+    })
+    // file uploaded on cloudinary
+    console.log("file uploaded on cloudinary", response.url);
+    return response;
+
+  } catch (error) {
+    fs.unlinkSync(localFilePath); // remove file from local uploads folder
+    console.log("error while uploading on cloudinary", error);
+  }
+}
+
+export {uploadOnCloudinary};
