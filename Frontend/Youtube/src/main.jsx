352ba056dfd49738e05ccc71f65263cdb1eb772d
Souma061 <soumabrataghosh57@gmail.com>
2025-10-31 20:33:57 +0530

    feat: Add header component and set up routing with react-router in main file

--- Frontend/Youtube/src/main.jsx
+++ Frontend/Youtube/src/main.jsx
@@ -1,7 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import {Provider} from 'react-redux';
+import {RouterProvider, createBrowserRouter} from 'react-router-dom';
 import App from './App.jsx'
+
+const router = createBrowserRouter([
+  {
+     
+  }
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
